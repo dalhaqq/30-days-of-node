fdb0874145a15902dfbf2a91ca9f915e90fc4625
dalhaqq <abdalhaqqm@gmail.com>
2021-01-10 13:44:04 +0700

    console.log()

---
+++ day-4/1-console-log.js
@@ -0,0 +1,12 @@
+//day-4/1-console-log.js
+
+console.log("1 : Hello world!");
+console.log('2 : This will also work');
+
+var str = '3 : nodejsera';
+var val = 25;
+
+console.log(str);
+console.log('4 : Value of val is %d', val);
+console.log('5 : %s', 'this is string formatted');
+console.log('6 : str = ' + str);
