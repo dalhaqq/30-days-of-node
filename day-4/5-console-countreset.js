a09d3b9ec1ce5b49d183e8a29dc274e10f5f0f98
dalhaqq <abdalhaqqm@gmail.com>
2021-01-10 18:00:19 +0700

    console.countReset()

---
+++ day-4/5-console-countreset.js
@@ -0,0 +1,9 @@
+//day-4/5-console-countreset.js
+
+console.count('default');
+console.count('remo');
+console.count('rj');
+console.countReset('remo');
+console.count('remo');
+console.count('rj');
+console.count();
