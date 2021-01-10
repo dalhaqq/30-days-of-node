565de888abd333b8766308d8b23662901089a1b2
dalhaqq <abdalhaqqm@gmail.com>
2021-01-10 18:03:08 +0700

    console.error()

---
+++ day-4/6-console-error.js
@@ -0,0 +1,11 @@
+//day-4/6-console-error.js
+
+var x = 10;
+var y = 20;
+var result = y/x;
+
+if(result == 2){
+    console.log("Result : %d", result);
+}else{
+    console.error("Error : Error in Positioning Operands");
+}
