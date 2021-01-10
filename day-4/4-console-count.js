37c24f2ad0a561bca126568a2d1dbfb7e5cd5107
dalhaqq <abdalhaqqm@gmail.com>
2021-01-10 17:58:25 +0700

    console.count()

---
+++ day-4/4-console-count.js
@@ -0,0 +1,9 @@
+//day-4/4-console-count.js
+
+console.count('default');
+console.count('remo');
+console.count('rj');
+console.count('remo');
+console.count('remo');
+console.count('rj');
+console.count();
